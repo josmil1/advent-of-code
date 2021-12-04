96b404a5d97eaa3dcac1cc4f946e88c48deecc2b
Jose Emilio Munoz Lopez <3432773+josmil1@users.noreply.github.com>
2021-12-04 13:37:02 +0000

    Completed Puzzle 2 part 2

--- Advent2021/Puzzle02/Puzzle02_2.cpp
+++ Advent2021/Puzzle02/Puzzle02_2.cpp
@@ -2,67 +2,47 @@
 
 void puzzle_02_2()
 {
-    std::ifstream in_file("/Users/josmil17/Programming/advent21/Advent2021/Advent2021/Puzzle01/input_0.txt");
+    std::ifstream in_file("/Users/josmil17/Programming/advent21/Advent2021/Advent2021/Puzzle02/input_0.txt");
     if (!in_file)
     {
         std::cerr << "Cannot open file";
         return;
     }
 
-    // Parse input
-    std::string line;
-    uint32_t increase_count = 0;
-    constexpr uint32_t window_size = 3;
-    uint32_t count = 0, sum_index = 0;
-    std::vector<uint32_t> sums;
-    sums.resize(window_size);
+    std::string line, command;
+    uint32_t number;
+    int64_t position = 0, depth = 0, aim = 0;
+
     while (std::getline(in_file, line))
     {
         if (line.size() > 0)
         {
-            uint32_t input_number = std::stoi(line);
+            // Parse input
+            std::stringstream line_stream{line};
+            line_stream >> command >> number;
 
-            if (count < window_size)
+            // Process commands
+            if ("forward" == command)
             {
-                // Initialise sums
-                for (int i = 0; i <= count; i++)
-                {
-                    sums[i] += input_number;
-                    //std::cout << "sum " << i << " + " << input_number << " = " << sums[i] << std::endl;
-                }
+                position += number;
+                depth += aim * number;
+            }
+            else if ("up" == command)
+            {
+                aim -= number;
+            }
+            else if ("down" == command)
+            {
+                aim += number;
             }
             else
             {
-                // Calculate differences and update sums
-                sum_index = (sum_index > window_size - 2) ? 0 : sum_index + 1;
-                const uint32_t prev_sum_index = (sum_index == 0) ? window_size - 1 : sum_index - 1;
-                
-                for (int i = 0; i < window_size; i++)
-                {
-                    // Add to all sums except the previous one, which will be
-                    // set to the new number at the end
-                    if (i != prev_sum_index)
-                    {
-                        sums[i] += input_number;
-                    }
-                    //std::cout << "sums " << i << " ( " << sums[i] << ") + " << input_number << " = " << sums[i] + input_number << std::endl;
-                }
-
-                const int32_t difference = sums[sum_index] - sums[prev_sum_index];
-                std::cout << "sums[" << sum_index << "] (" << sums[sum_index] << ") - sums[" << prev_sum_index << "] (" << sums[prev_sum_index] << ") = " << difference << std::endl;
-                if (difference > 0)
-                {
-                    increase_count++;
-                }
-
-                sums[prev_sum_index] = input_number;
+                std::cerr << "unknown command " << command << std::endl;
             }
-
-            count++;
         }
     }
 
-    std::cout << increase_count << std::endl;
+    std::cout << depth * position << std::endl;
 
     in_file.close();
 }
