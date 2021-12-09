8345cba677b309583f164695a9118f7848c92dec
Jose Emilio Munoz Lopez <3432773+josmil1@users.noreply.github.com>
2021-12-09 22:25:18 +0000

    Completed Puzzle 08 part 1

--- Advent2021/cpp/Puzzle08.cpp
+++ Advent2021/cpp/Puzzle08.cpp
@@ -1,20 +1,55 @@
 #include "Puzzles.hpp"
 
+#define UNIQUE_PATTERNS 10
+#define OUTPUT_DIGITS 4
+
 namespace
 {
     uint64_t solve(std::ifstream &in_file, bool part1)
     {
-        // Parse input
+        // 0 : 6
+        // 1 : 2
+        // 2 : 5
+        // 3 : 5
+        // 4 : 4
+        // 5 : 5
+        // 6 : 6
+        // 7 : 3
+        // 8 : 7
+        // 9 : 6
         std::string line;
+        uint32_t p1_answer = 0;
         while (std::getline(in_file, line))
         {
             if (line.size() > 0)
             {
-                std::cout << line << std::endl;
+                // Parse input
+                std::vector<std::string> patterns;
+                std::vector<std::string> output;
+                std::stringstream ss(line);
+                std::string str;
+                for (uint32_t i = 0; i < UNIQUE_PATTERNS; i++)
+                {
+                    ss >> str;
+                    patterns.push_back(str);
+                    //std::cout << str << std::endl;
+                }
+                ss >> str; // Discard '|' separator
+                // For part 1, count unique segment counts
+                for (uint32_t i = 0; i < OUTPUT_DIGITS; i++)
+                {
+                    ss >> str;
+                    output.push_back(str);
+                    //std::cout << str << std::endl;
+                    if ((str.length() > 1 && str.length() < 5) || str.length() == 7)
+                    {
+                        p1_answer++;
+                    }
+                }
             }
         }
 
-        return 0;
+        return p1_answer;
     }
 }
 
