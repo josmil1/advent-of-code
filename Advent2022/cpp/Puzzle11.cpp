b1c45df7e2ecc4ae5af6a533bb12b70d58f7000d
Jose Emilio Munoz Lopez <3432773+josmil1@users.noreply.github.com>
2022-12-12 01:14:00 +0000

    2022 Day 11

--- Advent2022/cpp/Puzzle11.cpp
+++ Advent2022/cpp/Puzzle11.cpp
@@ -12,7 +12,7 @@ struct Monkey
 	uint32_t              div;
 	uint32_t              true_id;
 	uint32_t              false_id;
-	uint32_t              inspection_count;
+	uint64_t              inspection_count;
 };
 
 using MonkeyMap = std::unordered_map<uint32_t, Monkey>;
@@ -56,12 +56,12 @@ MonkeyMap get_monkeys(std::ifstream &in_file)
 			ss = std::stringstream(line);
 			ss >> bin_str >> bin_str >> bin_str >> new_monkey.div;
 
-			// If true: throw to monkey 2
+			// If true: throw to monkey <true_id>
 			std::getline(in_file, line);
 			ss = std::stringstream(line);
 			ss >> bin_str >> bin_str >> bin_str >> bin_str >> bin_str >> new_monkey.true_id;
 
-			// If false: throw to monkey 3
+			// If false: throw to monkey <false_id>
 			std::getline(in_file, line);
 			ss = std::stringstream(line);
 			ss >> bin_str >> bin_str >> bin_str >> bin_str >> bin_str >> new_monkey.false_id;
@@ -73,7 +73,25 @@ MonkeyMap get_monkeys(std::ifstream &in_file)
 	return monkeys;
 }
 
-uint64_t get_new_worry(Monkey &monkey, uint32_t old_worry)
+void print_monkeys(MonkeyMap &monkeys)
+{
+	for (auto m : monkeys)
+	{
+		auto &monkey = m.second;
+		std::cout << "Monkey " << monkey.id << ": ";
+		for (auto i : monkey.items)
+		{
+			std::cout << i << " ";
+		}
+		std::cout << std::endl;
+		std::cout << "\tInspected " << monkey.inspection_count << " items" << std::endl;
+		std::cout << "\tOperation " << monkey.factor_a << monkey.op << monkey.factor_b << std::endl;
+		std::cout << "\tDivisible by " << monkey.div << std::endl;
+		std::cout << "\tTrue: " << monkey.true_id << " False: " << monkey.false_id << std::endl;
+	}
+}
+
+uint64_t get_new_worry(Monkey &monkey, uint64_t old_worry)
 {
 	uint64_t new_worry{0};
 	uint64_t factor_a = (monkey.factor_a == "old") ? old_worry : std::stol(monkey.factor_a);
@@ -101,14 +119,25 @@ uint64_t get_new_worry(Monkey &monkey, uint32_t old_worry)
 	return new_worry;
 }
 
-uint64_t puzzle_11_1(std::ifstream &in_file)
+uint64_t solve(std::ifstream &in_file, bool part_1)
 {
 	auto monkeys = get_monkeys(in_file);
 
-	constexpr uint32_t relief{3};
-	constexpr uint32_t rounds{20};
+	uint32_t rounds = part_1 ? 20 : 10000;
+	uint64_t relief{1};
+	if (part_1)
+	{
+		relief = 3;
+	}
+	else
+	{
+		// Common denominator
+		for (auto &monkey : monkeys)
+		{
+			relief *= monkey.second.div;
+		}
+	}
 
-	uint32_t most_inspections{0}, second_most_inspections{0};
 	for (size_t r = 0; r < rounds; r++)
 	{
 		for (size_t i = 0; i < monkeys.size(); i++)
@@ -118,7 +147,18 @@ uint64_t puzzle_11_1(std::ifstream &in_file)
 			// Game round
 			for (auto item : monkey.items)
 			{
-				auto new_worry = get_new_worry(monkey, item) / relief;
+				// Reduce worry
+				auto new_worry = get_new_worry(monkey, item);
+				if (part_1)
+				{
+					new_worry = new_worry / relief;
+				}
+				else
+				{
+					new_worry = new_worry % relief;
+				}
+
+				// Throw item
 				if (new_worry % monkey.div == 0)
 				{
 					monkeys[monkey.true_id].items.push_back(new_worry);
@@ -128,67 +168,39 @@ uint64_t puzzle_11_1(std::ifstream &in_file)
 					monkeys[monkey.false_id].items.push_back(new_worry);
 				}
 
-				monkey.inspection_count++;
+				monkey.inspection_count += 1;
 			}
 
 			monkey.items.clear();
+		}
+	}
 
-			// Monkey business
-			if (monkey.inspection_count > second_most_inspections)
+	// Find two highest inspection counts
+	uint64_t most_inspections{0}, second_most_inspections{0};
+	for (auto &m : monkeys)
+	{
+		auto &monkey = m.second;
+		if (monkey.inspection_count > second_most_inspections)
+		{
+			if (monkey.inspection_count > most_inspections)
 			{
-				if (monkey.inspection_count > most_inspections)
-				{
-					second_most_inspections = most_inspections;
-					most_inspections        = monkey.inspection_count;
-				}
-				else
-				{
-					second_most_inspections = monkey.inspection_count;
-				}
+				second_most_inspections = most_inspections;
+				most_inspections        = monkey.inspection_count;
+			}
+			else
+			{
+				second_most_inspections = monkey.inspection_count;
 			}
 		}
 	}
 
 	uint64_t monkey_business = most_inspections * second_most_inspections;
 
-	// DEBUG
-	/*
-	for (auto m : monkeys)
-	{
-	    auto &monkey = m.second;
-	    std::cout << "Monkey " << monkey.id << ": ";
-	    for (auto i : monkey.items)
-	    {
-	        std::cout << i << " ";
-	    }
-	    std::cout << std::endl;
-	    std::cout << "\tInspected " << monkey.inspection_count << " items" << std::endl;
-	    std::cout << "\tOperation " << monkey.factor_a << monkey.op << monkey.factor_b << std::endl;
-	    std::cout << "\tDivisible by " << monkey.div << std::endl;
-	    std::cout << "\tTrue: " << monkey.true_id << " False: " << monkey.false_id << std::endl;
-	}
-	*/
-
 	return monkey_business;
-}
-
-uint64_t puzzle_11_2(std::ifstream &in_file)
-{
-	std::string line;
-
-	while (std::getline(in_file, line))
-	{
-		if (line.size() > 0)
-		{
-			std::cout << line << std::endl;
-		}
-	}
-
-	return 0;
 }
 }        // namespace
 
 uint64_t puzzle_11(std::ifstream &in_file)
 {
-	return puzzle_11_1(in_file);
+	return solve(in_file, true);
 }
