99dfe76241bdd94fff871fa93b4058dc341bf26b
Jose Emilio Munoz Lopez <3432773+josmil1@users.noreply.github.com>
2023-12-01 19:33:16 +0000

    Day 1

--- Advent2023/cpp/Puzzle01.cpp
+++ Advent2023/cpp/Puzzle01.cpp
@@ -1,84 +1,115 @@
 #include "Puzzles.hpp"
 
-#include <set>
-
 namespace
 {
 uint64_t puzzle_01_1(std::ifstream &in_file)
 {
-	std::string line;
-
-	uint64_t max     = 0;
-	uint64_t cur_sum = 0;
+	std::string        line;
+	uint64_t           sum         = 0;
+	constexpr uint32_t digit_count = 2;
 
 	while (!in_file.eof())
 	{
 		std::getline(in_file, line);
+		std::string digits;
 
-		if (line.size() > 0)
+		for (auto c : line)
 		{
-			int64_t input_number = std::stol(line);
-			cur_sum += input_number;
+			int digit = c - '0';
+			if (digit < 10)
+			{
+				if (digits.size() == digit_count)
+				{
+					digits.pop_back();
+				}
+				digits += c;
+			}
 		}
 
-		if (line.size() == 0 || in_file.eof())
+		// 1 digit case
+		if (digits.size() < digit_count)
 		{
-			max     = std::max(max, cur_sum);
-			cur_sum = 0;
+			digits += digits[0];
 		}
+
+		sum += std::stol(digits);
 	}
 
-	return max;
+	return sum;
 }
 
 uint64_t puzzle_01_2(std::ifstream &in_file)
 {
-	std::string line;
-
-	constexpr uint32_t top_elf_count = 3;
-
-	std::vector<uint64_t> top_elf_sums;
-	uint64_t              cur_sum{0};
+	std::string              line;
+	uint64_t                 sum{0};
+	std::vector<std::string> string_digits{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
 	while (!in_file.eof())
 	{
 		std::getline(in_file, line);
+		std::string digits{"00"};
 
-		if (line.size() > 0)
+		// Find string digits
+		int last_pos{-1}, first_pos{std::numeric_limits<int>::max()};
+
+		for (int d = 0; d < string_digits.size(); d++)
 		{
-			int64_t input_number = std::stol(line);
-			cur_sum += input_number;
-		}
+			int new_digit = (d + 1) + '0';
 
-		if (line.size() == 0 || in_file.eof())
-		{
-			if (top_elf_sums.size() < top_elf_count)
+			// First
+			int pos = line.find(string_digits[d]);
+			if (pos != std::string::npos)
 			{
-				top_elf_sums.push_back(cur_sum);
-			}
-			else
-			{
-				std::sort(top_elf_sums.begin(), top_elf_sums.end(), std::less<>());
-
-				for (uint32_t i = 0; i < top_elf_count; i++)
+				if (pos < first_pos)
 				{
-					if (cur_sum > top_elf_sums[i])
-					{
-						top_elf_sums[i] = cur_sum;
-						break;
-					}
+					first_pos    = pos;
+					digits.at(0) = new_digit;
 				}
 			}
 
-			cur_sum = 0;
+			// Last
+			pos = line.rfind(string_digits[d]);
+			if (pos != std::string::npos)
+			{
+				if (pos > last_pos)
+				{
+					last_pos     = pos;
+					digits.at(1) = new_digit;
+				}
+			}
 		}
+
+		// Find numeric digits and replace if they occur before/after string ones
+		for (int d = 0; d < line.size(); d++)
+		{
+			int new_digit = line[d] - '0';
+
+			if (new_digit < 10)
+			{
+				// First
+				if (d < first_pos)
+				{
+					digits.at(0) = line[d];
+					first_pos    = d;
+				}
+
+				// Last
+				if (d > last_pos)
+				{
+					digits.at(1) = line[d];
+					last_pos     = d;
+				}
+			}
+		}
+
+		sum += std::stol(digits);
 	}
 
-	return std::accumulate(top_elf_sums.begin(), top_elf_sums.end(), 0);
+	return sum;
 }
 }        // namespace
 
 uint64_t puzzle_01(std::ifstream &in_file)
 {
-	return puzzle_01_1(in_file);
+	return puzzle_01_2(in_file);
 }
