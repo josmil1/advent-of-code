f236993ac5024ec0ec4c44d91c89e0fd5c493540
Jose Emilio Munoz Lopez <3432773+josmil1@users.noreply.github.com>
2023-12-05 10:48:10 +0000

    Day 5 Part 2 Slow

--- Advent2023/cpp/Puzzle05.cpp
+++ Advent2023/cpp/Puzzle05.cpp
@@ -23,7 +23,7 @@ class SeedMap
 
 		for (auto &mapping : mappings)
 		{
-			if (input >= mapping.src && input <= mapping.src + mapping.range)
+			if (input >= mapping.src && input < mapping.src + mapping.range)
 			{
 				output = mapping.dst + (input - mapping.src);
 				break;
@@ -38,8 +38,6 @@ class SeedMap
 };
 
 using Seedmap = std::unordered_map<uint64_t, uint64_t>;
-
-std::vector<uint64_t> seeds;
 
 SeedMap seedToSoil;
 SeedMap soilToFertilizer;
@@ -68,14 +66,42 @@ inline void parse_map(std::ifstream &in_file, SeedMap &seed_map)
 
 void parse_maps(std::ifstream &in_file)
 {
-	seeds.clear();
-
 	std::string line;
 	std::string str;
 	uint64_t    num;
 
+	// Seed to Soil
+	parse_map(in_file, seedToSoil);
+	parse_map(in_file, soilToFertilizer);
+	parse_map(in_file, fertilizerToWater);
+	parse_map(in_file, waterToLight);
+	parse_map(in_file, lightToTemperature);
+	parse_map(in_file, temperatureToHumidity);
+	parse_map(in_file, humidityToLocation);
+}
+
+uint64_t seed_to_location(uint64_t seed)
+{
+	auto soil        = seedToSoil.get(seed);
+	auto fertilizer  = soilToFertilizer.get(soil);
+	auto water       = fertilizerToWater.get(fertilizer);
+	auto light       = waterToLight.get(water);
+	auto temperature = lightToTemperature.get(light);
+	auto humidity    = temperatureToHumidity.get(temperature);
+	auto location    = humidityToLocation.get(humidity);
+
+	return location;
+}
+
+uint64_t puzzle_05_1(std::ifstream &in_file)
+{
 	// Seeds
+	std::vector<uint64_t> seeds;
 	{
+		std::string line;
+		std::string str;
+		uint64_t    num;
+
 		std::getline(in_file, line);
 		std::stringstream ss(line);
 
@@ -88,31 +114,13 @@ void parse_maps(std::ifstream &in_file)
 		std::getline(in_file, line);        // Empty
 	}
 
-	// Seed to Soil
-	parse_map(in_file, seedToSoil);
-	parse_map(in_file, soilToFertilizer);
-	parse_map(in_file, fertilizerToWater);
-	parse_map(in_file, waterToLight);
-	parse_map(in_file, lightToTemperature);
-	parse_map(in_file, temperatureToHumidity);
-	parse_map(in_file, humidityToLocation);
-}
-
-uint64_t puzzle_05_1(std::ifstream &in_file)
-{
 	parse_maps(in_file);
 
 	uint64_t lowest_location{std::numeric_limits<uint64_t>::max()};
 
 	for (auto &seed : seeds)
 	{
-		auto soil        = seedToSoil.get(seed);
-		auto fertilizer  = soilToFertilizer.get(soil);
-		auto water       = fertilizerToWater.get(fertilizer);
-		auto light       = waterToLight.get(water);
-		auto temperature = lightToTemperature.get(light);
-		auto humidity    = temperatureToHumidity.get(temperature);
-		auto location    = humidityToLocation.get(humidity);
+		auto location = seed_to_location(seed);
 
 		lowest_location = std::min(lowest_location, location);
 	}
@@ -122,21 +130,47 @@ uint64_t puzzle_05_1(std::ifstream &in_file)
 
 uint64_t puzzle_05_2(std::ifstream &in_file)
 {
-	std::string line;
-
-	while (std::getline(in_file, line))
+	// Seeds
+	std::vector<std::pair<uint64_t, uint64_t>> seed_ranges;
 	{
-		if (line.size() > 0)
+		std::string line;
+		std::string str;
+		uint64_t    start, range;
+
+		std::getline(in_file, line);
+		std::stringstream ss(line);
+
+		ss >> str;
+		while (ss >> start >> range)
 		{
-			std::cout << line << std::endl;
+			seed_ranges.push_back({start, range});
+		}
+
+		std::getline(in_file, line);        // Empty
+	}
+
+	parse_maps(in_file);
+
+	uint64_t lowest_location{std::numeric_limits<uint64_t>::max()};
+
+	for (auto &seed_range : seed_ranges)
+	{
+		std::cout << "Processing range " << seed_range.first << std::endl;
+
+		for (uint64_t i = 0; i < seed_range.second; i++)
+		{
+			auto seed     = seed_range.first + i;
+			auto location = seed_to_location(seed);
+
+			lowest_location = std::min(lowest_location, location);
 		}
 	}
 
-	return 0;
+	return lowest_location;
 }
 }        // namespace
 
 uint64_t puzzle_05(std::ifstream &in_file)
 {
-	return puzzle_05_1(in_file);
+	return puzzle_05_2(in_file);
 }
