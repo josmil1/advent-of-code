a6c97a2b9e4a9bfaab728ba879130829d0dfba37
Jose Emilio Munoz Lopez <3432773+josmil1@users.noreply.github.com>
2023-12-05 07:58:39 +0000

    Day 4 Part 2 Slow

--- Advent2023/cpp/Puzzle04.cpp
+++ Advent2023/cpp/Puzzle04.cpp
@@ -56,21 +56,64 @@ uint64_t puzzle_04_1(std::ifstream &in_file)
 
 uint64_t puzzle_04_2(std::ifstream &in_file)
 {
+	std::unordered_map<uint32_t, uint32_t> card_count;
+
 	std::string line;
 
 	while (std::getline(in_file, line))
 	{
 		if (line.size() > 0)
 		{
-			std::cout << line << std::endl;
+			// Parse card and check winning (repeat) numbers
+			std::stringstream ss(line);
+
+			int         card_id;
+			std::string str;
+			uint32_t    num;
+
+			// Card <id>:
+			ss >> str >> card_id >> str;
+			card_count[card_id]++;
+
+			std::vector<uint32_t> number_list;
+			while (ss >> str)
+			{
+				if (str != "|")
+				{
+					num = static_cast<uint32_t>(std::stol(str));
+					number_list.push_back(num);
+				}
+			}
+
+			for (int i = 0; i < card_count[card_id]; i++)
+			{
+				std::unordered_map<uint32_t, bool> numbers;
+				uint64_t                           count{0};
+
+				for (auto &num : number_list)
+				{
+					if (numbers.count(num))
+					{
+						// Number appeared in winning list
+						count++;
+						card_count[card_id + count]++;
+					}
+					else
+					{
+						// New number
+						numbers[num] = true;
+					}
+				}
+			}
 		}
 	}
 
-	return 0;
+	return std::accumulate(card_count.begin(), card_count.end(), 0,
+	                       [](uint32_t value, const std::unordered_map<uint32_t, uint32_t>::value_type &p) { return value + p.second; });
 }
 }        // namespace
 
 uint64_t puzzle_04(std::ifstream &in_file)
 {
-	return puzzle_04_1(in_file);
+	return puzzle_04_2(in_file);
 }
