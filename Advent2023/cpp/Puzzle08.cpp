bf0f06e45c762231847b7c868d0ec7fa90412d1e
Jose Emilio Munoz Lopez <3432773+josmil1@users.noreply.github.com>
2023-12-08 19:32:50 +0000

    Day 8 Part 2 Slow

--- Advent2023/cpp/Puzzle08.cpp
+++ Advent2023/cpp/Puzzle08.cpp
@@ -1,21 +1,59 @@
 #include "Puzzles.hpp"
 
+#include <numeric>
+
+// Lowest common multiple functions from https://stackoverflow.com/a/4229930
 namespace
 {
+uint64_t gcd(uint64_t a, uint64_t b)
+{
+	while (true)
+	{
+		if (0 == a)
+		{
+			return b;
+		}
+
+		b %= a;
+
+		if (0 == b)
+		{
+			return a;
+		}
+
+		a %= b;
+	}
+}
+
+uint64_t lcm(uint64_t a, uint64_t b)
+{
+	uint64_t temp = gcd(a, b);
+
+	return temp ? (a / temp * b) : 0;
+}
+
+uint64_t lcm_vector(std::vector<uint64_t> v)
+{
+	uint64_t result = std::accumulate(v.begin(), v.end(), 1ULL, lcm);
+
+	return result;
+}
+
 struct Location
 {
 	std::string left;
 	std::string right;
 };
 
-uint64_t puzzle_08_1(std::ifstream &in_file)
+using LocationMap = std::unordered_map<std::string, Location>;
+
+std::vector<std::string> start_locations;
+
+LocationMap parse_map(std::ifstream &in_file)
 {
+	LocationMap locations;
+
 	std::string line;
-
-	std::string                               sequence;
-	std::unordered_map<std::string, Location> locations;
-
-	std::getline(in_file, sequence);
 
 	while (std::getline(in_file, line))
 	{
@@ -28,8 +66,27 @@ uint64_t puzzle_08_1(std::ifstream &in_file)
 			ss >> start >> bin >> left >> right;
 
 			locations[start] = {left.substr(1, 3), right.substr(0, 3)};
+
+			// For Part 2 only
+			if ('A' == start[2])
+			{
+				start_locations.push_back(start);
+			}
 		}
 	}
+
+	return locations;
+}
+
+uint64_t puzzle_08_1(std::ifstream &in_file)
+{
+	std::string line;
+
+	std::string sequence;
+
+	std::getline(in_file, sequence);
+
+	auto locations = parse_map(in_file);
 
 	const std::string origin = "AAA";
 	const std::string goal   = "ZZZ";
@@ -63,19 +120,79 @@ uint64_t puzzle_08_2(std::ifstream &in_file)
 {
 	std::string line;
 
-	while (std::getline(in_file, line))
+	std::string sequence;
+
+	std::getline(in_file, sequence);
+
+	auto locations = parse_map(in_file);
+
+	std::vector<uint64_t> steps(start_locations.size(), 0);
+
+	for (uint64_t i = 0; i < start_locations.size(); i++)
 	{
-		if (line.size() > 0)
+		uint64_t step_index = 0;
+
+		while ('Z' != start_locations[i][2])
 		{
-			std::cout << line << std::endl;
+			auto current_index = step_index % sequence.size();
+			char current_step  = sequence[current_index];
+
+			auto current_options = locations[start_locations[i]];
+
+			if ('R' == current_step)
+			{
+				start_locations[i] = current_options.right;
+			}
+			else
+			{
+				start_locations[i] = current_options.left;
+			}
+
+			step_index++;
 		}
+
+		steps[i] = step_index;
 	}
 
-	return 0;
+	return lcm_vector(steps);
+	/*
+	while (true)
+	{
+	    auto current_index = step_index % sequence.size();
+	    char current_step  = sequence[current_index];
+
+	    bool stop = true;
+
+	    for (uint64_t i = 0; i < start_locations.size(); i++)
+	    {
+	        auto l = start_locations[i];
+
+	        auto current_options = locations[l];
+
+	        if ('R' == current_step)
+	        {
+	            start_locations[i] = current_options.right;
+	        }
+	        else
+	        {
+	            start_locations[i] = current_options.left;
+	        }
+
+	        stop &= ('Z' == start_locations[i][2]);
+	    }
+
+	    step_index++;
+
+	    if (stop)
+	    {
+	        break;
+	    }
+	}
+	*/
 }
 }        // namespace
 
 uint64_t puzzle_08(std::ifstream &in_file)
 {
-	return puzzle_08_1(in_file);
+	return puzzle_08_2(in_file);
 }
