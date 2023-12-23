99d2acd57b5ed608c454bb39c24841e2dd0913b8
Jose Emilio Munoz Lopez <3432773+josmil1@users.noreply.github.com>
2023-12-23 01:43:20 +0100

    Day 17 Slow

--- Advent2023/README.md
+++ Advent2023/README.md
@@ -1,6 +1,9 @@
 # Advent of Code 2023
 https://adventofcode.com/2023
 
+## Day 17
+* [Dijkstra's algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
+
 ## Day 18
 * [Even–odd rule](https://en.wikipedia.org/wiki/Even–odd_rule)
 * [Pick's theorem](https://en.m.wikipedia.org/wiki/Pick%27s_theorem)
--- Advent2023/cpp/Puzzle17.cpp
+++ Advent2023/cpp/Puzzle17.cpp
@@ -1,5 +1,7 @@
 #include "Puzzles.hpp"
 
+#include <deque>
+
 namespace
 {
 struct Pos
@@ -14,35 +16,6 @@ struct Pos
 	}
 };
 
-struct Node
-{
-	Pos      pos{};
-	int      heat{0};
-	bool     visited;
-	uint32_t min_distance{std::numeric_limits<uint32_t>::max()};
-	Node    *prev_node{nullptr};
-	int      same_direction_count{0};
-};
-
-using Grid = std::unordered_map<uint64_t, Node>;
-
-struct HeatMap
-{
-	Grid     grid;
-	uint32_t width{0};
-	uint32_t height{0};
-};
-
-inline uint64_t key(uint32_t x, uint32_t y)
-{
-	return static_cast<uint64_t>(x) << 32 | y;
-}
-
-inline uint64_t key(Pos pos)
-{
-	return key(static_cast<uint32_t>(pos.x), static_cast<uint32_t>(pos.y));
-}
-
 enum Direction : uint16_t
 {
 	Right = 0,
@@ -51,13 +24,35 @@ enum Direction : uint16_t
 	Up    = 3,
 };
 
-struct NodeState
+struct HeatCell
+{
+	Pos       pos{};
+	int64_t   heat{0};
+	Direction dir;
+	int32_t   same_direction_count{0};
+};
+
+using Grid = std::unordered_map<uint64_t, HeatCell>;
+
+struct HeatMap
+{
+	Grid    grid;
+	int32_t width{0};
+	int32_t height{0};
+};
+
+inline uint64_t key(int32_t x, int32_t y)
+{
+	return static_cast<int64_t>(x) << 32 | y;
+}
+
+struct CellState
 {
 	Pos       pos{};
 	Direction dir{};
 	int       same_direction_count{0};
 
-	bool operator==(const NodeState &other) const
+	bool operator==(const CellState &other) const
 	{
 		return pos == other.pos &&
 		       dir == other.dir &&
@@ -65,24 +60,192 @@ struct NodeState
 	}
 };
 
-struct NodeStateHasher
+struct CellStateHasher
 {
-	std::size_t operator()(const NodeState &n) const
+	std::size_t operator()(const CellState &c) const
 	{
 		using std::hash;
 		using std::size_t;
 		using std::string;
 
-		return ((hash<string>()(std::to_string(key(n.pos))) ^ (hash<int>()(n.dir) << 1)) >> 1) ^
-		       (hash<int>()(n.same_direction_count) << 1);
+		return ((hash<string>()(std::to_string(key(c.pos.x, c.pos.y))) ^ (hash<int>()(c.dir) << 1)) >> 1) ^
+		       (hash<int>()(c.same_direction_count) << 1);
 	}
 };
 
-uint64_t puzzle_17_1(std::ifstream &in_file)
+uint64_t get_min_heat(HeatMap &heatmap, uint32_t min_cells = 0, uint32_t max_cells = 0)
 {
-	std::string line;
+	int64_t min_heat{std::numeric_limits<int64_t>::max()};
 
-	HeatMap heatmap;
+	// Dijkstra's
+	std::unordered_map<CellState, uint64_t, CellStateHasher> visited;
+
+	const Pos target_pos = Pos{heatmap.width - 1, heatmap.height - 1};
+
+	std::deque<HeatCell> queue;
+	queue.push_back({{0, 0}, 0, Direction::Right, 1});
+	queue.push_back({{0, 0}, 0, Direction::Down, 1});
+
+	while (!queue.empty())
+	{
+		// Pop node with minimum heat
+		int64_t  temp_min_heat{std::numeric_limits<int64_t>::max()};
+		uint32_t min_index;
+		for (int v = 0; v < queue.size(); v++)
+		{
+			auto node = queue[v];
+			if (node.heat < temp_min_heat)
+			{
+				temp_min_heat = node.heat;
+				min_index     = v;
+			}
+		}
+		auto node = queue[min_index];
+		queue.erase(queue.begin() + min_index);
+
+		// Track not only distance to this node but also from which direction
+		// and momentum (how many steps in same direction)
+		if (visited.count({node.pos, node.dir, node.same_direction_count}))
+		{
+			continue;
+		}
+		visited[{node.pos, node.dir, node.same_direction_count}] = node.heat;
+
+		// Update the next node in the same direction
+		Pos next_pos = node.pos;
+		switch (node.dir)
+		{
+			case Direction::Up:
+			{
+				next_pos.y--;
+				break;
+			}
+			case Direction::Down:
+			{
+				next_pos.y++;
+				break;
+			}
+			case Direction::Right:
+			{
+				next_pos.x++;
+				break;
+			}
+			case Direction::Left:
+			{
+				next_pos.x--;
+				break;
+			}
+			default:
+			{
+				std::cout << "Error" << std::endl;
+			}
+		}
+
+		auto next_min_heat = node.heat + heatmap.grid[key(next_pos.x, next_pos.y)].heat;
+
+		if (next_pos == target_pos)
+		{
+			if (min_cells == 0 || node.same_direction_count >= min_cells)
+			{
+				// Reached target with correct number of steps in the same direction
+				min_heat = std::min(min_heat, next_min_heat);
+				continue;
+			}
+		}
+
+		// Update neighbors of next node
+		std::deque<Direction> next_dirs;
+
+		if (min_cells > 0 && node.same_direction_count < min_cells)
+		{
+			// Force same direction
+			next_dirs.push_back(node.dir);
+		}
+		else
+		{
+			// Allow direction change
+			next_dirs.push_back(node.dir);
+
+			switch (node.dir)
+			{
+				case Direction::Up:
+				case Direction::Down:
+				{
+					next_dirs.push_back(Direction::Left);
+					next_dirs.push_back(Direction::Right);
+					break;
+				}
+				case Direction::Left:
+				case Direction::Right:
+				{
+					next_dirs.push_back(Direction::Up);
+					next_dirs.push_back(Direction::Down);
+					break;
+				}
+				default:
+				{
+					std::cout << "Error" << std::endl;
+				}
+			}
+		}
+
+		// Force direction change
+		if (max_cells > 0 && node.same_direction_count >= max_cells)
+		{
+			next_dirs.pop_front();
+		}
+
+		for (auto &next_dir : next_dirs)
+		{
+			auto new_next_pos = next_pos;
+			switch (next_dir)
+			{
+				case Direction::Up:
+				{
+					new_next_pos.y--;
+					break;
+				}
+				case Direction::Down:
+				{
+					new_next_pos.y++;
+					break;
+				}
+				case Direction::Right:
+				{
+					new_next_pos.x++;
+					break;
+				}
+				case Direction::Left:
+				{
+					new_next_pos.x--;
+					break;
+				}
+				default:
+				{
+					std::cout << "Error" << std::endl;
+				}
+			}
+
+			// Bounds check
+			if (new_next_pos.x >= 0 && new_next_pos.x < heatmap.width &&
+			    new_next_pos.y >= 0 && new_next_pos.y < heatmap.height)
+			{
+				// Valid position to update next
+				auto new_same_direction_count = next_dir != node.dir ? 1 : node.same_direction_count + 1;
+
+				queue.push_back({next_pos, next_min_heat, next_dir, new_same_direction_count});
+			}
+		}
+	}
+
+	return min_heat;
+}
+
+HeatMap parse_heatmap(std::ifstream &in_file)
+{
+	HeatMap heatmap{};
+
+	std::string line;
 
 	while (std::getline(in_file, line))
 	{
@@ -96,215 +259,32 @@ uint64_t puzzle_17_1(std::ifstream &in_file)
 			for (int32_t i = 0; i < heatmap.width; i++)
 			{
 				Pos pos{i, static_cast<int32_t>(heatmap.height)};
-				heatmap.grid[key(pos)] = {pos, line[i] - '0'};
+				heatmap.grid[key(pos.x, pos.y)] = {pos, line[i] - '0'};
 			}
 
 			heatmap.height++;
 		}
 	}
 
-	auto &source        = heatmap.grid[key(0, 0)];
-	source.min_distance = 0;
+	return heatmap;
+}
 
-	std::vector<Node *> nodes_with_distance;
-	nodes_with_distance.push_back(&source);
+uint64_t puzzle_17_1(std::ifstream &in_file)
+{
+	auto heatmap = parse_heatmap(in_file);
 
-	std::unordered_map<NodeState, bool, NodeStateHasher> visited;
-
-	while (!nodes_with_distance.empty())
-	{
-		uint32_t min_distance{std::numeric_limits<uint32_t>::max()};
-		uint32_t min_index;
-		for (int v = 0; v < nodes_with_distance.size(); v++)
-		{
-			auto node = nodes_with_distance[v];
-			if (!node->visited && node->min_distance < min_distance)
-			{
-				min_distance = node->min_distance;
-				min_index    = v;
-			}
-		}
-
-		auto node = nodes_with_distance[min_index];
-		// node->visited = true;
-
-		NodeState state;
-		state.pos                  = node->pos;
-		state.same_direction_count = node->same_direction_count + 1;
-
-		std::cout << "Processing node at (" << node->pos.x << ", " << node->pos.y << ")" << std::endl;
-
-		Node *node_up{nullptr};
-		Node *node_down{nullptr};
-		Node *node_left{nullptr};
-		Node *node_right{nullptr};
-
-		if (node->pos.x - 1 >= 0)
-		{
-			node_left = &heatmap.grid[key(node->pos.x - 1, node->pos.y)];
-		}
-		if (node->pos.x + 1 < heatmap.width)
-		{
-			node_right = &heatmap.grid[key(node->pos.x + 1, node->pos.y)];
-		}
-		if (node->pos.y - 1 >= 0)
-		{
-			node_up = &heatmap.grid[key(node->pos.x, node->pos.y - 1)];
-		}
-		if (node->pos.y + 1 < heatmap.height)
-		{
-			node_down = &heatmap.grid[key(node->pos.x, node->pos.y + 1)];
-		}
-
-		// Define 'forward' as left-to-right in x-axis
-		Node *forward_node{nullptr};
-		Node *left_node{nullptr};
-		Node *right_node{nullptr};
-
-		if (!node->prev_node || node->prev_node->pos.y == node->pos.y)
-		{
-			// Coming from left/right (default at start)
-			bool coming_from_left = node->prev_node ? node->prev_node->pos.x < node->pos.x : true;
-
-			left_node    = coming_from_left ? node_up : node_down;
-			forward_node = coming_from_left ? node_right : node_left;
-			right_node   = coming_from_left ? node_down : node_up;
-
-			state.dir = coming_from_left ? Direction::Right : Direction::Left;
-		}
-		else if (node->prev_node->pos.x == node->pos.x)
-		{
-			// Coming from above/below
-			bool coming_from_above = node->prev_node->pos.y < node->pos.y;
-
-			left_node    = coming_from_above ? node_right : node_left;
-			forward_node = coming_from_above ? node_down : node_up;
-			right_node   = coming_from_above ? node_left : node_right;
-
-			state.dir = coming_from_above ? Direction::Down : Direction::Up;
-		}
-
-		std::vector<Node *> connected;
-		connected.push_back(forward_node);
-		connected.push_back(left_node);
-		connected.push_back(right_node);
-		for (auto n : connected)
-		{
-			if (n && !visited.count({n->pos, state.dir, n->same_direction_count}) &&
-			    node->min_distance != std::numeric_limits<uint32_t>::max())
-			{
-				bool smaller_distance = node->min_distance + n->heat < n->min_distance;
-				if (smaller_distance && state.same_direction_count < 4)
-				{
-					n->min_distance         = node->min_distance + n->heat;
-					n->prev_node            = node;
-					n->same_direction_count = state.same_direction_count;
-
-					auto it = std::find(nodes_with_distance.begin(), nodes_with_distance.end(), n);
-					if (it == nodes_with_distance.end())
-					{
-						std::cout << "\t\tAdd node at (" << n->pos.x << ", " << n->pos.y << ")" << std::endl;
-						nodes_with_distance.push_back(n);
-					}
-				}
-			}
-		}
-
-		/*
-		        for (auto n : connected)
-		        {
-		            if (n)
-		            {
-		                std::cout << "\tLooking at (" << n->pos.x << ", " << n->pos.y << ") - "
-		                          << node->min_distance << " + " << n->heat << " < " << n->min_distance << "?" << std::endl;
-		            }
-
-		            if (n && !n->visited && node->min_distance != std::numeric_limits<uint32_t>::max())
-		            {
-		                bool two_consecutive = (node->prev_node &&
-		                                        n->pos.x == node->prev_node->pos.x) ||
-		                                       (node->prev_node &&
-		                                        n->pos.y == node->prev_node->pos.y);
-
-		                bool three_consecutive = two_consecutive &&
-		                                         ((node->prev_node->prev_node &&
-		                                           n->pos.x == node->prev_node->prev_node->pos.x) ||
-		                                          (node->prev_node->prev_node &&
-		                                           n->pos.y == node->prev_node->prev_node->pos.y));
-
-		                bool four_consecutive = three_consecutive &&
-		                                        ((node->prev_node->prev_node->prev_node &&
-		                                          n->pos.x == node->prev_node->prev_node->prev_node->pos.x) ||
-		                                         (node->prev_node->prev_node->prev_node &&
-		                                          n->pos.y == node->prev_node->prev_node->prev_node->pos.y));
-
-		                bool smaller_distance          = node->min_distance + n->heat < n->min_distance;
-		                bool equal_or_smaller_distance = node->min_distance + n->heat <= n->min_distance;
-
-		                if (((smaller_distance && !four_consecutive) ||
-		                     (equal_or_smaller_distance && !three_consecutive) ||
-		                     (equal_or_smaller_distance && !two_consecutive)))
-		                {
-		                    n->min_distance = node->min_distance + n->heat;
-		                    n->prev_node    = node;
-
-		                    auto it = std::find(nodes_with_distance.begin(), nodes_with_distance.end(), n);
-		                    if (it == nodes_with_distance.end())
-		                    {
-		                        std::cout << "\t\tAdd node at (" << n->pos.x << ", " << n->pos.y << ")" << std::endl;
-		                        nodes_with_distance.push_back(n);
-		                    }
-		                }
-		                else
-		                {
-		                    std::cout << "\t\tfour_consecutive" << std::endl;
-		                }
-		            }
-		            else
-		            {
-		                if (n && n->visited)
-		                {
-		                    std::cout << "\t\tAlready visited" << std::endl;
-		                }
-		            }
-		        }
-
-		*/
-
-		visited[state] = true;
-
-		nodes_with_distance.erase(nodes_with_distance.begin() + min_index);
-	}
-
-	auto node         = &heatmap.grid[key(heatmap.width - 1, heatmap.height - 1)];
-	auto min_distance = node->min_distance;
-
-	while (node->prev_node)
-	{
-		std::cout << node->prev_node->heat << std::endl;
-		node = node->prev_node;
-	}
-
-	return min_distance;
+	return get_min_heat(heatmap, 0, 3);
 }
 
 uint64_t puzzle_17_2(std::ifstream &in_file)
 {
-	std::string line;
+	auto heatmap = parse_heatmap(in_file);
 
-	while (std::getline(in_file, line))
-	{
-		if (line.size() > 0)
-		{
-			std::cout << line << std::endl;
-		}
-	}
-
-	return 0;
+	return get_min_heat(heatmap, 4, 10);
 }
 }        // namespace
 
 uint64_t puzzle_17(std::ifstream &in_file)
 {
-	return puzzle_17_1(in_file);
+	return puzzle_17_2(in_file);
 }
