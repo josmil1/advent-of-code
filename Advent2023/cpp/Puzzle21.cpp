970d8cc285a4eddd94420899a7dba93977034b16
Jose Emilio Munoz Lopez <3432773+josmil1@users.noreply.github.com>
2023-12-23 02:56:18 +0100

    Day 21 Part 2 Slow

--- Advent2023/cpp/Puzzle21.cpp
+++ Advent2023/cpp/Puzzle21.cpp
@@ -128,84 +128,15 @@ void print(Garden &garden, std::vector<Pos> *final_positions = nullptr)
 	}
 }
 
-uint64_t puzzle_21_1(std::ifstream &in_file)
-{
-	auto garden = parse_garden(in_file);
-
-	Pos start = {garden.width / 2, garden.height / 2};
-
-	std::vector<Pos> final_positions;
-
-	constexpr int STEPS = 64;
-
-	// BFS
-	std::unordered_map<PlotState, bool, PlotStateHasher> visited;
-
-	std::deque<PlotState> positions;
-	positions.push_back({start, 0});
-
-	while (!positions.empty())
-	{
-		auto cur      = positions.front().pos;
-		auto distance = positions.front().distance;
-		positions.pop_front();
-
-		if (visited.count({cur, distance}) && visited[{cur, distance}])
-		{
-			continue;
-		}
-
-		visited[{cur, distance}] = true;
-
-		if (STEPS == distance)
-		{
-			final_positions.push_back(cur);
-		}
-		else
-		{
-			for (int i = -1; i <= 1; i++)
-			{
-				for (int j = -1; j <= 1; j++)
-				{
-					if ((i == 0 && j == 0) || (std::abs(i) == std::abs(j)))
-					{
-						// Orthogonal only, and not-self
-						continue;
-					}
-
-					auto new_pos = cur + Pos{i, j};
-
-					if (new_pos.x >= 0 && new_pos.x < garden.width &&
-					    new_pos.y >= 0 && new_pos.y < garden.height &&
-					    garden.grid.count(key(new_pos)))
-					{
-						// Within bounds, and not a rock: visit neighbors
-						positions.push_back({new_pos, distance + 1});
-					}
-				}
-			}
-		}
-	}
-
-	return final_positions.size();
-}
-
 int64_t mod(int64_t a, int64_t b)
 {
+	// Avoid negative remainder
 	return (a % b + b) % b;
 }
 
-uint64_t puzzle_21_2(std::ifstream &in_file)
+uint64_t count_positions(Garden &garden, Pos start, uint32_t exact_steps)
 {
-	auto garden = parse_garden(in_file);
-
-	Pos start = {garden.width / 2, garden.height / 2};
-
 	std::set<Pos> final_positions;
-
-	// constexpr int STEPS = 65 + 262;        // 94315
-	//  constexpr int STEPS = 65 + 131;        // 33976
-	constexpr int STEPS = 65;        // 3787
 
 	// BFS
 	std::unordered_map<PlotState, bool, PlotStateHasher> visited;
@@ -226,7 +157,7 @@ uint64_t puzzle_21_2(std::ifstream &in_file)
 
 		visited[{cur, distance}] = true;
 
-		if (STEPS == distance)
+		if (exact_steps == distance)
 		{
 			final_positions.insert(cur);
 		}
@@ -246,7 +177,8 @@ uint64_t puzzle_21_2(std::ifstream &in_file)
 
 					if (garden.grid.count(key(mod(new_pos.x, garden.width), mod(new_pos.y, garden.width))))
 					{
-						// Within bounds, and not a rock: visit neighbors
+						// No bounds-check, pattern repeats infinitely
+						// Not a rock: visit neighbors
 						positions.push_back({new_pos, distance + 1});
 					}
 				}
@@ -255,10 +187,66 @@ uint64_t puzzle_21_2(std::ifstream &in_file)
 	}
 
 	return final_positions.size();
-	// Fit polynomial: https://www.wolframalpha.com/input?i=quadratic+fit+calculator&assumption=%7B%22F%22%2C+%22QuadraticFitCalculator%22%2C+%22data3x%22%7D+-%3E%22%7B0%2C1%2C2%7D%22&assumption=%7B%22F%22%2C+%22QuadraticFitCalculator%22%2C+%22data3y%22%7D+-%3E%22%7B3787%2C+33976%2C+94315%7D%22
-	// Resolve: https://www.wolframalpha.com/input?i=3787+%2B+15114+x+%2B+15075+x%5E2%2C+x+%3D+202300
-	// Answer: 616951804315987
 }
+
+uint64_t puzzle_21_1(std::ifstream &in_file)
+{
+	auto garden = parse_garden(in_file);
+
+	Pos start = {garden.width / 2, garden.height / 2};
+
+	constexpr int STEPS = 6;        // 64 for real input
+
+	return count_positions(garden, start, STEPS);
+}
+
+uint64_t fit_and_solve_quadratic(int64_t y_1, int64_t y_2, int64_t y_3, int64_t x = 0)
+{
+	// Fit quadratic
+	// 3 equations, 3 unknowns
+	// a*x^2 + b*x + c
+
+	// At x_1 = 0
+	int64_t c = y_1;
+
+	// At x_2 = 1
+	// a + b + c = y_2
+	// a = y_2 - c - b
+
+	// At x_3 = 2
+	// 4a + 2b + c = y_3
+	// 4 * (y_2 - c - b) + 2b + c = y_3
+	// 4y_2 - 4c - 4b + 2b + c = y_3
+	// 4y_2 - 3c - 2b = y_3
+	// 2b = 4y_2 - 3c - y_3
+	int64_t b = ((4 * y_2) - (3 * c) - y_3) / 2;
+
+	int64_t a = y_2 - c - b;
+
+	// Solve quadratic for x
+	return a * x * x + b * x + c;
+}
+
+uint64_t puzzle_21_2(std::ifstream &in_file)
+{
+	auto garden = parse_garden(in_file);
+
+	Pos start = {garden.width / 2, garden.height / 2};
+
+	constexpr int STEPS = 26501365;
+	// Does not work with example input, requires:
+	// -> width = height
+	// -> middle row/column must be empty
+	int64_t div = STEPS / garden.width;
+	int64_t rem = STEPS % garden.width;
+
+	auto y_1 = count_positions(garden, start, rem);
+	auto y_2 = count_positions(garden, start, rem + garden.width);
+	auto y_3 = count_positions(garden, start, rem + garden.width * 2);
+
+	return fit_and_solve_quadratic(y_1, y_2, y_3, div);
+}
+
 }        // namespace
 
 uint64_t puzzle_21(std::ifstream &in_file)
