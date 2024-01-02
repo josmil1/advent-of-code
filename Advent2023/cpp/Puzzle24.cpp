079f9d8455e2df77644c27f13241b9c4843c10f0
Jose Emilio Munoz Lopez <3432773+josmil1@users.noreply.github.com>
2024-01-02 11:38:44 +0100

    Day 24 Clean-up

--- Advent2023/cpp/Puzzle24.cpp
+++ Advent2023/cpp/Puzzle24.cpp
@@ -23,62 +23,43 @@ struct Line
 	Pos diff{};
 };
 
-Pos get_intersection(Line a, Line b, bool &valid)
+bool get_intersection(Line a, Line b, Pos &intersection)
 {
-	Pos intersection{};
+	bool found_intersection = true;
 
-	long double x1 = a.u.x, x2 = a.v.x, x3 = b.u.x, x4 = b.v.x;
-	long double y1 = a.u.y, y2 = a.v.y, y3 = b.u.y, y4 = b.v.y;
+	long double x_1 = a.u.x, x_2 = a.v.x, x_3 = b.u.x, x_4 = b.v.x;
+	long double y_1 = a.u.y, y_2 = a.v.y, y_3 = b.u.y, y_4 = b.v.y;
 
-	auto d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-	// If d is zero, there is no intersection
-	if (d == 0)
+	auto d = (x_1 - x_2) * (y_3 - y_4) - (y_1 - y_2) * (x_3 - x_4);
+
+	if (d != 0)
 	{
-		std::cout << "No intersection" << std::endl;
-		valid = false;
-		return intersection;
+		long double pre  = (x_1 * y_2 - y_1 * x_2);
+		long double post = (x_3 * y_4 - y_3 * x_4);
+
+		intersection.x = (pre * (x_3 - x_4) - (x_1 - x_2) * post) / d;
+		intersection.y = (pre * (y_3 - y_4) - (y_1 - y_2) * post) / d;
+	}
+	else
+	{
+		found_intersection = false;
 	}
 
-	// Get the x and y
-	long double pre = (x1 * y2 - y1 * x2), post = (x3 * y4 - y3 * x4);
-	long double x = (pre * (x3 - x4) - (x1 - x2) * post) / d;
-	long double y = (pre * (y3 - y4) - (y1 - y2) * post) / d;
-
-	// Check if the x and y coordinates are within both lines
-	/*
-	if (x < std::min(x1, x2) || x > std::max(x1, x2) ||
-	    x < std::min(x3, x4) || x > std::max(x3, x4))
-	{
-	    std::cout << "Out of bounds at x";
-	    return intersection;
-	}
-	if (y < std::min(y1, y2) || y > std::max(y1, y2) ||
-	    y < std::min(y3, y4) || y > std::max(y3, y4))
-	{
-	    std::cout << "Out of bounds at y";
-	    return intersection;
-	}
-	*/
-
-	// Return the point of intersection
-	intersection.x = x;
-	intersection.y = y;
-	return intersection;
+	return found_intersection;
 }
 
 uint64_t puzzle_24_1(std::ifstream &in_file)
 {
 	std::vector<Line> hailstones;
 
+	constexpr bool example_input = true;
+
+	constexpr int64_t MIN = example_input ? 7 : 200000000000000;
+	constexpr int64_t MAX = example_input ? 27 : 400000000000000;
+
+	// Parse hailstones
 	std::string line;
 	char        c;
-
-	constexpr int64_t MIN = 200000000000000;
-	constexpr int64_t MAX = 400000000000000;
-
-	// constexpr int64_t MIN = 7;
-	// constexpr int64_t MAX = 27;
-
 	while (std::getline(in_file, line))
 	{
 		if (line.size() > 0)
@@ -112,7 +93,7 @@ uint64_t puzzle_24_1(std::ifstream &in_file)
 			if (hailstone.u.z < MIN && hailstone.diff.z < 0 ||
 			    hailstone.u.z > MAX && hailstone.diff.z > 0)
 			{
-				// continue;
+				continue;
 			}
 			hailstone.v.z = hailstone.u.z + hailstone.diff.z;
 
@@ -120,6 +101,7 @@ uint64_t puzzle_24_1(std::ifstream &in_file)
 		}
 	}
 
+	// Count intersections between every pair (ignore z-coordinates)
 	uint64_t intersection_count{0};
 	for (int i = 0; i < hailstones.size(); i++)
 	{
@@ -128,15 +110,9 @@ uint64_t puzzle_24_1(std::ifstream &in_file)
 			auto &a = hailstones[i];
 			auto &b = hailstones[j];
 
-			// std::cout << a.u.x << ":" << a.u.y << ":" << a.u.z << std::endl;
-			// std::cout << b.u.x << ":" << b.u.y << ":" << b.u.z << std::endl;
-
-			a.v = a.u + a.diff;
-			b.v = b.u + b.diff;
-
-			auto temp_a = a;
-			auto temp_b = b;
-
+			// Scale the coordinates down to avoid overflow
+			auto temp_a  = a;
+			auto temp_b  = b;
 			auto divisor = std::max(a.u.x, b.u.x);
 
 			temp_a.u.x = a.u.x / divisor;
@@ -155,20 +131,19 @@ uint64_t puzzle_24_1(std::ifstream &in_file)
 			temp_b.v.y = b.v.y / divisor;
 			temp_b.v.z = b.v.z / divisor;
 
-			// std::cout << temp_a.u.x << ":" << temp_a.u.y << ":" << temp_a.u.z << std::endl;
-			// std::cout << temp_b.u.x << ":" << temp_b.u.y << ":" << temp_b.u.z << std::endl;
+			// Find intersection
+			Pos  intersection{};
+			auto found_intersection = get_intersection(temp_a, temp_b, intersection);
 
-			bool valid        = true;
-			auto intersection = get_intersection(temp_a, temp_b, valid);
+			// Scale intersection coordinates back up
 			intersection.x *= divisor;
 			intersection.y *= divisor;
 
-			// std::cout << "Intersection " << intersection.x << ":" << intersection.y << std::endl;
-
-			if (valid &&
+			if (found_intersection &&
 			    intersection.x >= MIN && intersection.x <= MAX &&
 			    intersection.y >= MIN && intersection.y <= MAX)
 			{
+				// Within bounds
 				if (((a.u.x < a.v.x && intersection.x >= a.u.x) ||
 				     (a.u.x > a.v.x && intersection.x <= a.u.x)) &&
 				    ((b.u.x < b.v.x && intersection.x >= b.u.x) ||
@@ -178,32 +153,19 @@ uint64_t puzzle_24_1(std::ifstream &in_file)
 				    ((b.u.y < b.v.y && intersection.y >= b.u.y) ||
 				     (b.u.y > b.v.y && intersection.y <= b.u.y)))
 				{
-					// std::cout << "intersection at " << intersection.x << ", " << intersection.y << std::endl;
+					// After the hailstone start position
 					intersection_count++;
 				}
 			}
-
-			// std::cout << std::endl;
 		}
 	}
 
-	// 11577 too low
-	// 10918 too low
 	return intersection_count;
 }
 
 uint64_t puzzle_24_2(std::ifstream &in_file)
 {
-	std::string line;
-
-	while (std::getline(in_file, line))
-	{
-		if (line.size() > 0)
-		{
-			std::cout << line << std::endl;
-		}
-	}
-
+	// Solved externally with Z3 Theorem Prover
 	return 0;
 }
 }        // namespace
